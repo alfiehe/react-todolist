44ab83562b0c55071c8ad0bcab38f4a083232dbb
alfiehe <alfiehe@tencent.com>
2021-01-14 00:23:25 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+
+  // tips: hook 必须初始化react 组件的内部。你不能在函数外部初始化它
+  const [todos, setTodos] = React.useState([
+    {
+      content: "Pickup dry cleaning",
+      isCompleted: true,
+    },
+    {
+      content: "Get haircut",
+      isCompleted: false,
+    },
+    {
+      content: "Build a todo app in React",
+      isCompleted: false,
+    },
+  ]);
+
+  function handleKeyDown(e, i) {
+    if(e.key === 'Enter') {
+      console.log(e.key, i);
+      createTodoAtIndex(e, i);
+    }
+    if (e.key === 'Backspace' && todos[i].content === '') {
+      e.preventDefault();
+      return removeTodoAtIndex(i);
+    }
+  }
+
+  function createTodoAtIndex(e, i) {
+    const newTodos = [...todos];
+    newTodos.splice(i + 1, 0, {
+      content: '',
+      isCompleted: false,
+    });
+    setTodos(newTodos);
+    setTimeout(() => {
+      document.forms[0].elements[i + 1].focus();
+    }, 0);
+  }
+
+  function updateTodoAtIndex(e, i) {
+    const newTodos = [...todos];
+    newTodos[i].content = e.target.value;
+    setTodos(newTodos);
+  }
+
+  function removeTodoAtIndex(i) {
+    if (i === 0 && todos.length === 1) return;
+    setTodos(todos => todos.slice(0, i).concat(todos.slice(i + 1, todos.length)));
+    setTimeout(() => {
+      document.forms[0].elements[i - 1].focus();
+    }, 0);
+  }
+
+  function toggleTodoCompleteAtIndex(index) {
+    const temporaryTodos = [...todos];
+    temporaryTodos[index].isCompleted = !temporaryTodos[index].isCompleted;
+    setTodos(temporaryTodos);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="header">
+        <img src={logo} className="logo" alt="logo" />
       </header>
+      <form className="todo-list">
+        <ul>
+          {todos.map((todo, i) => (
+            <div className={`todo ${todo.isCompleted && 'todo-is-completed'}`} key={i}>
+              <div className={'checkbox'} onClick={() => toggleTodoCompleteAtIndex(i)}>
+                {todo.isCompleted && (
+                  <span>&#x2714;</span>
+                )}
+              </div>
+              <input
+                type="text"
+                value={todo.content}
+                onKeyDown={e => handleKeyDown(e, i)} 
+                onChange={e => updateTodoAtIndex(e, i)}
+              />
+            </div>
+          ))}
+        </ul>
+      </form>
     </div>
   );
 }
